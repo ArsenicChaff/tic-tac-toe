e339a0274dbf2ec0e3806407be5394c7a0f86999
Jacob Lowe <jacob@Jacobs-MacBook-Pro.local>
2025-02-27 11:10:45 -0500

    {python} fixed various bugs

--- in-python/main.py
+++ in-python/main.py
@@ -21,9 +21,12 @@ def print_game(game_board):
 def initialize_game():
     #this is a script to begin the game, assign player names to X and O
     start = input('Hello! Welcome to Tic Tac Toe.\nWould you like to play a game? yes[y]/no[n]\n')
-    if start.lower().strip() == 'n':
-        print("Exiting program")
-        exit()
+    while start != 'y':
+        if start.lower().strip() == 'n':
+            print("Exiting program")
+            exit()
+        quit_check(start.lower().strip())
+        start = input("Sorry, I didnt catch that. Please enter 'y' to play, or 'n' to exit.")
 
     print("Quit the game at any point by entering 'Q'.")
     p1 = input("The first player will play as X! What is your name?\n")
@@ -49,42 +52,42 @@ def prompt_move(player):
     quit_check(move)
     return move_array
 
-
-p1, p2 = initialize_game()
-game = game_state(p1, p2)
-print_game(game.game_board)
-
-#initializing variables
-player_turn_check = 2
-player_whos_turn = p1
-player_move = [0, 0] #col, row
-succsesful_move = False
-winner = [False, 'winner_symbol']
-
-while winner[0] == False:
-    if player_turn_check % 2 == 0:
-        player_whos_turn = p1
-    else:
-        player_whos_turn = p2
-
-    while succsesful_move == False:
-        player_move = prompt_move(player_whos_turn)
-        succsesful_move = game.make_move(player_whos_turn, player_move)
-    
-    if succsesful_move == True:
-        player_turn_check += 1
-    
-    if player_turn_check >= 11: # 11 because variable initialized to 2 plus 9 moves
-        winner = [True, 'tie']
-    elif player_turn_check >= 7: # 7 because variable initialized to 2 plus 5 moves minimum before a winner is possible
-        winner = game.check_win()
-
+if __name__ == "__main__":
+    p1, p2 = initialize_game()
+    game = game_state(p1, p2)
     print_game(game.game_board)
 
-    succsesful_move = False #reset value to prompt for turn again
+#initializing variables
+    player_turn_check = 2
+    player_whos_turn = p1
+    player_move = [0, 0] #col, row
+    succsesful_move = False
+    winner = [False, 'winner_symbol']
 
-if winner[1] == 'tie':
-    print("It's a Tie! Play again to see who wins.")
-else:
-    player_winner = p1 if winner[1] == 'X' else p2
+    while winner[0] == False:
+        if player_turn_check % 2 == 0:
+            player_whos_turn = p1
+        else:
+            player_whos_turn = p2
+
+        while succsesful_move == False:
+            player_move = prompt_move(player_whos_turn)
+            succsesful_move = game.make_move(player_whos_turn, player_move)
+    
+        if succsesful_move == True:
+            player_turn_check += 1
+    
+        if player_turn_check >= 11: # 11 because variable initialized to 2 plus 9 moves
+            winner = [True, 'tie']
+        elif player_turn_check >= 7: # 7 because variable initialized to 2 plus 5 moves minimum before a winner is possible
+            winner = game.check_win()
+
+        print_game(game.game_board)
+
+        succsesful_move = False #reset value to prompt for turn again
+
+    if winner[1] == 'tie':
+        print("It's a Tie! Play again to see who wins.")
+    else:
+        player_winner = p1 if winner[1] == 'X' else p2
     print(f"Congrats! Player {player_winner} wins! Rerun the program to play again.")
